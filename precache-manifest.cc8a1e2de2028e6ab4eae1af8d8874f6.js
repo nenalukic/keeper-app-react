2c3f893db61c04c5baeefc7a20ca71269d7f0530
Nevenka Lukic <nenamilkov@gmail.com>
2022-02-09 15:57:48 +0100

    Updates

---
+++ precache-manifest.cc8a1e2de2028e6ab4eae1af8d8874f6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "436b94c938ce1fdf0e25fc946c411f2a",
+    "url": "/keeper-app-react/index.html"
+  },
+  {
+    "revision": "4d03261ec178348e5e20",
+    "url": "/keeper-app-react/static/js/2.44d2c861.chunk.js"
+  },
+  {
+    "revision": "c9a7b464710e41b5d404",
+    "url": "/keeper-app-react/static/js/main.e176d282.chunk.js"
+  },
+  {
+    "revision": "1db221600dd07618913d",
+    "url": "/keeper-app-react/static/js/runtime-main.c4300277.js"
+  }
+]);
